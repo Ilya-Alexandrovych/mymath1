15e179b42b6c462a10a740f17cb5aee317562c8a
Ilya-Alexandrovych <127094141+Ilya-Alexandrovych@users.noreply.github.com>
2023-09-02 18:02:50 +0300

    Add files via upload

---
+++ mymath.go
@@ -0,0 +1,54 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Pow(x float64, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Max(x float64, y float64) float64 {
+	return math.Max(x, y)
+}
+func Min(x float64, y float64) float64 {
+	return math.Min(x, y)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+func IsInf(f float64, sign int) bool {
+
+	return math.IsInf(f, sign)
+}
+
+func IsNaN(f float64) (is bool) {
+	return math.IsNaN(f)
+}
